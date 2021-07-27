33bbecc56f64774477f6fdad015ff7af56265faa
Leevardaro <Leevardaro@gmail.com>
2021-07-27 17:52:30 -0400

    try

---
+++ src/test/java/BrowserStackMaven/BSM/AppTestTest.java
@@ -0,0 +1,11 @@
+package BrowserStackMaven.BSM;
+
+import org.testng.annotations.Test;
+
+public class AppTestTest {
+
+  @Test
+  public void shouldAnswerWithTrueTest() {
+    throw new RuntimeException("Test not implemented");
+  }
+}
